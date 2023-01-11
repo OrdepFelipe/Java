75daecfd6f1d3a45e3b1e43941f08d8006ccf4d4
Pedro Felipe Abreu <88599118+OrdepFelipe@users.noreply.github.com>
2023-01-10 22:08:54 -0300

    Add files via upload

--- Funcionario.java
+++ Funcionario.java
@@ -4,12 +4,10 @@ public class Funcionario {
 	private Integer id;
 	private String nome;
 	private Double salario;
-	
-	
-	
+
 	public Funcionario() {
 	}
-	
+
 	public Funcionario(int id, String nome, double salario) {
 
 		this.id = id;
@@ -20,28 +18,29 @@ public class Funcionario {
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public double getSalario() {
 		return salario;
 	}
+
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
 
-	
 	public String toString() {
-		return "\nId: " + id + 
-				"\nNome: " + nome 
-				+"\nSalario: " + salario;
+		return "\nId: " + id + "\nNome: " + nome + "\nSalario: " + salario;
 	}
-	
-}
 
+}
--- Program.java
+++ Program.java
@@ -38,29 +38,30 @@ public class Program {
 			if (verifica.getId() == procuraFuncionario) {
 				System.out.println("\nFuncionario selecionado:");
 				System.out.println(verifica.toString());
-				
+
 				System.out.println("\nDigite 1 - AUMENTAR SALARIO 2 - DIMINUIR SALARIO");
 				int opcSelecionada = sc.nextInt();
+				
 				if (opcSelecionada == 1) {
 
 					System.out.println("Informe de quantos porcentos será o incremento no salario");
 					Double varSalario = sc.nextDouble();
 
 					VariacaoSalario varSalarioFuncionario = new VariacaoSalario(varSalario);
-					verifica.setSalario(varSalarioFuncionario.incrementoSalario(salario));// altero o salario atraves do
-																								// metodo incremetoSalario
-					System.out.println(verifica.toString());}// printa os dados do funcionario com salario atualizado
-					 else {
+					verifica.setSalario(varSalarioFuncionario.incrementoSalario(salario));// altero o salario atraves do  metodo incremetoSalario
+					System.out.println(verifica.toString());
+				} // printa os dados do funcionario com salario atualizado
+				else {
 
-							System.out.println("Informe de quantos porcentos será o decremento no salario");
-							Double varSalario = sc.nextDouble();
+					System.out.println("Informe de quantos porcentos será o decremento no salario");
+					Double varSalario = sc.nextDouble();
 
-							VariacaoSalario varSalarioFuncionario = new VariacaoSalario(varSalario);
-							verifica.setSalario(varSalarioFuncionario.decrementoSalario(salario));
-							System.out.println(verifica.toString());
-						}
+					VariacaoSalario varSalarioFuncionario = new VariacaoSalario(varSalario);
+					verifica.setSalario(varSalarioFuncionario.decrementoSalario(salario));
+					System.out.println(verifica.toString());
 				}
 			}
+		}
 		sc.close();
 	}
 
